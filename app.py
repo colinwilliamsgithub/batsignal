fe34d4a917800ac78a4f5e497546f24ffb0a2e40
Tommy MItchell <98601714+Shruggly@users.noreply.github.com>
2024-03-05 11:47:00 +0000

    Co-authored-by: colinwilliamsgithub <colinwilliamsgithub@users.noreply.github.com> changed text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Bat Signal Recognizer!"
+    return "Welcome to the BatSignal!"
 
 if __name__ == '__main__':
     app.run(debug=True)
